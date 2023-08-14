20fce3d7b2e6d7e257a7361641caabd60c98b209
Romic361 <111034854+Romic361@users.noreply.github.com>
2023-08-14 18:52:21 -0300

    nuevos archivos

---
+++ listas.py
@@ -0,0 +1,28 @@
+Familia=["Emma Sofia", "Marisa Beatriz", "Hector Fabian", "Joaquin C", "Gonzalo C", "Nicolas C", "Graciela Beatriz", "Gladys Dominga", "Pedro Francisco", "Osvaldo Antonia"]
+
+Febrero2023= [27,27,27,30,29,29,32,32,31,32,34,37,37,30,34,29,26,16,24,26,28,30,33,32,30,30,26,28,33]
+
+Ciudades= ["Monte lindo", "La Rioja", "Nono", "Mina Clavero", "Rosario", "Florianopolis"]
+
+Eventos= ["egreso secuendario-12/2012", "AbuelaF- 07/2020", "Nacimiento Emma- 10/09/18", "Nacimiento Pauli- 03/11/2021", "Renuncia trabajo- 04/2020" ]
+
+Familia.sort()
+print(Familia)
+
+Febrero2023.sort()
+print(Febrero2023)
+
+Febrero2023.extend([33,34,32,35,30,32,31,31,35,34,37,37,36,37,35])
+print(Febrero2023)
+
+Familia[6:10]
+print(Familia)
+
+Ciudades.remove(0)
+print(Ciudades)
+
+print(Familia)
+print(Ciudades)
+print(Febrero2023)
+print(Eventos)
+
---
+++ tuplas.py
@@ -0,0 +1,9 @@
+Nala= ("perra", "gris", "Rosario", "intensa", 3, 2023, 23.5)
+
+Daysi=("perra", "amarillo", "verano", "noche", 12, 2012, 15.2)
+
+Humma= ("perra", "marroncito", "murcielago", 11, 2013, 10.4 )
+
+
+tuplas=[Nala, Daysi, Humma]
+
